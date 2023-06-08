af71aaf57b6fcb09a96408ac57f1c6908d4cd18e
rosa2108 <rosannyguzman@gmail.com>
2023-06-07 22:55:58 -0300

    Des. Usuario UseCase + UsuarioController

---
+++ controllers/usuario_controller.js
@@ -0,0 +1,73 @@
+import { UsuarioUseCase } from "../use_cases/usuarios.js";
+
+class UsuarioController {
+  constructor() {
+    this.usuarioUseCase = new UsuarioUseCase();
+  }
+  //CONTROLLER CREAR USUARIO 
+  async crearController(req, res, next) {
+    req.body = { nombre, apellido, mail, direccion, telefono, password };
+    const usuario = req.body;
+
+    try {
+      await usuarioUseCase.crear(usuario);
+      res.status(200).json({ mensaje: "Usuario Creado Exitosamente" });
+    } catch (error) {
+      res.status(500);
+      next(error);
+    }
+  }
+
+  //CONTROLLER LISTAR USUARIOS
+  async listarController(req, res) {
+    try {
+      const respuesta = await this.usuarioUseCase.listar();
+      res.status(200);
+      res.send("Usuarios Registrados" + respuesta);
+    } catch (error) {
+      res.status(500);
+    }
+  }
+
+  //CONTROLLER BUSCAR USUARIO POR MAIL
+  async buscarController(req,res){
+    const {id} = req.params;
+    
+    try{
+        const respuesta = await this.usuarioUseCase.buscar(id);
+        res.status(200).json(respuesta)
+    }catch(error){
+        res.status(500)
+        res.send("Usuario Inexistente")
+    }
+  }
+
+  //CONTROLLER PARA ELIMINAR USUARIO
+  async eliminarUsuario(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      await this.usuarioUseCase.eliminar(id);
+      res.status(200).json({ mensaje: "Usuario eliminado correctamente" });
+    } catch (error) {
+        res.status(500);
+      next(error);
+    }
+  }
+ 
+  //CONTROLLER PARA MODIFICAR
+  async modificarController(req, res) {
+    const { id, telefono} = req.params;
+
+    try {
+      await this.usuarioUseCase.modificar(id, telefono);
+      res.status(200).json({ mensaje: "Usuario modificado exitosamente" });
+    } catch (error) {
+      res.status(500);
+    }
+  }
+
+
+}
+
+
---
+++ use_cases/usuarios.js
@@ -0,0 +1,51 @@
+import { UsuarioFactory } from "../factories/usuario_factory.js";
+import { UsuarioRepository } from "../repository/usuario_repository.js";
+
+class UsuarioUseCase {
+  async crear(nombre, apellido, mail, direccion, telefono, password) {
+    try {
+      //CREAMOS EL USUARIO CORRESPONDIENTE VERIFICADO POR EXCEPCION
+      const usuario = new UsuarioFactory().crear(nombre,apellido,mail,direccion,telefono,password);
+
+      //CREAMOS CONST REPOSITORIO Y GUARDAMOS AL USUARIO CREADO
+      const resRespositorio = new UsuarioRepository();
+      await resRespositorio.guardar(usuario);
+
+    } catch (e) {
+      console.error("Usuario Invalido");
+    }
+  }
+
+  //LISTAR LOS USUARIOS 
+  async listar(){
+    await (new UsuarioRepository().listarUsuarios())
+  }
+
+  //BUSCAR USUARIO POR MAIL 
+  async buscar(id){
+    const usuarioBuscado = await (new UsuarioRepository().buscarUsuario(id))
+    if(!usuarioBuscado){
+        throw new Error ("Usuario no encontrado")
+    }
+    return usuarioBuscado
+  }
+
+  //ELIMINAR  USUARIO
+  async eliminar(id){
+    const eliminado = await new UsuarioRepository().eliminarUsuario(id)
+
+    if (eliminado.deleteCount == 0) {
+        throw new Error ("El usuario se eliminó correctamente.");
+        //resultado.result.deletedCount
+      } 
+  }
+
+  //MODIFICAR USUARIO
+  async modificar(mail, telfNuevo){
+    const modificado = await new UsuarioRepository().modificarTelefono(mail,telfNuevo)
+     if(modificado.modifiedCount == 0){
+        throw new  Error ("No se pudo modificar el telelfono")
+     }
+  }
+
+}
