54d567b6e833a0b068a5f1e6f6331d9fc3180841
Joaquin Espiñeira <joaespi@hotmail.com>
2023-06-21 19:09:22 -0300

    Test arreglo en servicio

--- test/servicio.spec.js
+++ test/servicio.spec.js
@@ -7,7 +7,7 @@ describe('Servicio', () => {
   // Crea una prueba para el contstructor
   describe('#constructor()', () => {
     it('debe crear un objeto Servicio con los valores proporcionados', function() {
-        const servicio = new Servicio('Gasista', 'Instalación y reparación de sistemas de gas', 1566962698, 'Juan B. Justo 2500', true);
+        const servicio = new Servicio('0011', 'Gasista', 'Instalación y reparación de sistemas de gas', 1566962698, 'Juan B. Justo 2500', true);
         expect(servicio).to.be.an.instanceOf(Servicio);
         expect(servicio.nombre).to.equal('Gasista');
         expect(servicio.descripcion).to.include('gas');
@@ -19,7 +19,7 @@ describe('Servicio', () => {
   // Crea una prueba para el método getDescripcion()
   describe('#getDescripcion', () => {
     it('Debe devolver la descripción del servicio', () => {
-      const servicio2 = new Servicio('Electricista', 'Instalación y reparación de sistemas eléctricos', 1555874588, 'Urquiza 560', true);
+      const servicio2 = new Servicio('Electricista', 1555874588, 'Instalación y reparación de sistemas eléctricos', 'Urquiza 560', true);
       expect(servicio2.getDescripcion()).to.equal('Instalación y reparación de sistemas eléctricos');
     });
   });
